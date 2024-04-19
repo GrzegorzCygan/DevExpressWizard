0a2ec61163adb949f32a6f62a5dc20088e721a2e
GrzegorzCygan <grzegorz.cygan.2023@gmail.com>
2024-04-19 18:56:59 +0200

    Add project files.

---
+++ DevExpressWizard/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+
+namespace DevExpressWizard.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        protected IDocumentManagerService DocumentManagerService { get { return GetService<IDocumentManagerService>(); } }
+
+        [Command]
+        public void OpenWizard(object o)
+        {
+            var document = DocumentManagerService.CreateDocument("Wizard", new WizardViewModel());
+            document.Title = "wizard";
+            document.Show();
+        }
+    }
+}
---
+++ DevExpressWizard/ViewModels/WizardViewModel.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+
+namespace DevExpressWizard.ViewModels
+{
+    public class WizardViewModel : ViewModelBase
+    {
+        public enum Page
+        {
+            First,
+            Second,
+            Third
+        }
+        private string _first;
+        public string First
+        {
+            get
+            {
+                return _first;
+            }
+            set
+            {
+                _first = value;
+                RaisePropertiesChanged(nameof(First), nameof(CanGoNext));
+            }
+        }
+        private string _second;
+        public string Second
+        {
+            get
+            {
+                return _second;
+            }
+            set
+            {
+                _second = value;
+                RaisePropertiesChanged(nameof(Second), nameof(CanGoNext));
+            }
+        }
+        private string _third;
+        public string Third
+        {
+            get
+            {
+                return _third;
+            }
+            set
+            {
+                _third = value;
+                RaisePropertiesChanged(nameof(Third), nameof(CanGoNext));
+            }
+        }
+        public bool CanGoNext
+        {
+            get 
+            {
+                return IsValid((Page)SelectedIndex);
+            }
+        }
+        private int _selectedIndex;
+        public int SelectedIndex { 
+            get
+            {
+                return _selectedIndex;
+            }
+            set
+            {
+                _selectedIndex = value;
+                RaisePropertiesChanged(nameof(SelectedIndex), nameof(CanGoNext));
+            }
+        }
+        [Command]
+        public void Finish()
+        {
+
+        }
+        private bool IsValid(Page page)
+        {
+            if (page == Page.First)
+            {
+                return !string.IsNullOrEmpty(_first);
+            }
+            else if (page == Page.Second)
+            {
+                return !string.IsNullOrEmpty(_second);
+            }
+            else if (page == Page.Third)
+            {
+                return !string.IsNullOrEmpty(_third);
+            }
+            return false;
+        }
+    }
+}
